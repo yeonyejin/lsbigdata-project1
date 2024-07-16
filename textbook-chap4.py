f0c0720ffab283898e9200903b93a8997f18192e
yeon <dusdpdlk@gmail.com>
2024-07-16 09:13:26 +0900

    hw1

--- textbook-chap4.py
+++ textbook-chap4.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 import numpy as np
 
@@ -28,3 +30,83 @@ sum(df['판매량']) / 3
 
 
 
+pd.show_versions()
+
+#df[("name", "english")]
+
+pd.show_versions()
+
+!pip install openpyxl
+
+import pandas as pd
+
+df_exam = pd.read_excel("data/excel_exam.xlsx")
+df_exam 
+
+
+
+sum(df_exam["math"]) / 20
+sum(df_exam["english"]) /20
+sum(df_exam["science"]) /20
+
+len(df_exam)
+df_exam.shape
+df_exam.size
+
+
+df_exam
+
+
+df_exam = pd.read_excel("data/excel_exam.xlsx", 
+                      sheet_name = "Sheet2")
+df_exam
+
+
+df_exam["total"] = df_exam["math"] + df_exam["english"] + df_exam["science"]
+df_exam
+
+df_exam["mean"] = (df_exam["math"] + df_exam["english"] + df_exam["science"]) / 3
+df_exam["mean"] = df_exam["total"] / 3
+
+
+df_exam["math"] > 50
+
+
+
+df_exam[df_exam["math"] > 50]
+
+df_exam[(df_exam["math"] > 50) & (df_exam["english"] > 50)]
+(df_exam["math"] > 50) & (df_exam["english"] > 50)
+
+
+import numpy as np
+
+
+mean_m = np.mean(df_exam["math"])
+mean_e = np.mean(df_exam["english"])
+
+df_exam[(df_exam["math"] > mean_m) & (df_exam["english"] < mean_e)]
+
+df_nc3 = df_exam[df_exam["nclass"] == 3]
+df_nc3[["math", "english", "science"]]
+df_nc3[0:1]
+df_nc3[1:2]
+df_nc3[1:5]
+
+df_exam[0:10]
+df_exam[7:16]
+a = np.array([4, 2, 5, 3, 6])
+a[2]
+
+df_exam[0:10:2]
+
+df_exam.sort_values("math", ascending=False)
+df_exam.sort_values(["nclass", "math"], ascending=[True,False])
+
+a >3
+np.where(a >3, "Up", "Down")
+np.where(df_exam["math"] > 50, "Up", "Down")
+
+
+
+df_exam["updown"] = np.where(df_exam["math"] > 50, "Up", "Down")
